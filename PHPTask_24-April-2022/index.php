272b383590dd2b4b13608c11c1857d15dd2fa07d
ashrafabueraq <ashrafabueraq97@gmail.com>
2022-04-25 23:10:00 +0300

    main

--- PHPTask_24-April-2022/index.php
+++ PHPTask_24-April-2022/index.php
@@ -1,11 +1,20 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php  
+$year = 2013;  
+ 
+if((0 == $year % 4) )
+{
+echo "$year is a Leap Year.";    
+}
+ 
+else  
+{  
+echo "$year is not a Leap Year.";    
+}
+?> 
+ 
+</body>
+</html>
