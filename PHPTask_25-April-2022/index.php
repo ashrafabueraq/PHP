cff8d94d34b0fac8863ad09b5f7e4fb869ef4ecd
ashrafabueraq <ashrafabueraq97@gmail.com>
2022-05-06 17:44:14 +0300

    main

--- PHPTask_25-April-2022/index.php
+++ PHPTask_25-April-2022/index.php
@@ -1,179 +1,241 @@
 
 <?php
 
-$year = 2022;
+function b(){
 
-
-if($year % 4 == 0){
-    echo "This year is leap";
-}else{
-    echo "This year is not leap";
+    echo ' <br>';
+    echo ' <br>';
 }
 
-echo "<br>";
-echo "<br>";
 
-$temperature = 18;
+function check_year($year){
 
-if($temperature < 20){
-    echo "It's winter time ";
-}else{
-    echo "It's summer time";
-}
+    if ($year % 400 == 0){
 
-echo "<br>";
-echo "<br>";
-
-$firstInteger = 2;
-$secondInteger = 2;
-$output = $firstInteger + $secondInteger;
-if($firstInteger == $secondInteger)
-    $output = $output * 3;
-
-echo $output;
-
-echo "<br>";
-echo "<br>";
-
-$firstInteger = 10;
-$secondInteger = 20;
-$output = $firstInteger + $secondInteger;
-if($output > 29){
- echo "true";
-}else{
- echo "false";
-}
-
-echo "<br>";
-echo "<br>";
-
-$firstInteger = 20;
-if($firstInteger % 3){
-    echo "false";
-}else{
-    echo "true";
-}
-
-echo "<br>";
-echo "<br>";
-
-
-if($firstInteger > 19 && $firstInteger < 51){
-    echo "true";
-}else{
-    echo "false";
-}
-
-echo "<br>";
-echo "<br>";
-
-
-$numbers = array(1, 9, 3);
-$max = $numbers[0];
-
-for($i = 1 ; $i <= 2; $i++)
-    if($numbers[$i] > $max)
-        $max = $numbers[$i];
+        echo 'this is leap year  ';
     
-echo $max;
+    }
+    else if ($year % 100 == 0){
+        echo ' this is not leap year ';
+    }
+    else if ($year % 4 == 0){
+    
+        echo ' this is a leap year';
+    
+    }else {
+        echo 'this is not leap year';
+    }
+
+}
+
+$year = 1900;
+check_year($year);
 
 
-echo "<br>";
-echo "<br>";
+b();
 
-$units = 300;
-$bill = 0.0;
+function check_temperature($temperature){
+    if ($temperature > 25 ){
 
-if($units != 0){
-    if($units < 51){
-        $bill = $units * 2.5;
+        echo 'It is summertime';
+
     }else{
-        $bill = 50 * 2.5;
-        if($units < 101 ){
-            $units -= 50;
-            $bill += $units * 5;
-        }else{
-            $bill += 50 * 5;
-            if($units < 251){
-                $units -= 100;
-                $bill += $units * 6.20;
-            }else{
-                $bill += 150 * 6.2;
-                if($units >= 251){
-                    $units -= 250;
-                    $bill += $units * 7.50;
-                }
-            }
-        }
+        echo ' It is wintertime';
     }
 }
 
-echo $bill;
+$temperature = 27;
+check_temperature($temperature);
+
+b();
 
 
-echo "<br>";
-echo "<br>";
+function check_same($num1 , $num2){
 
-$age = 18;
+    $sum = $num1 + $num2;
 
-if($age < 18){
-    echo "is not eligible to vote";
+   if($num1 == $num2){
+
+    echo $sum * 3;
+
+   }
+   else{
+    echo $sum;
+   }
+
+}
+
+
+check_same(2,2);
+
+
+
+b();
+
+
+function check_number($num1, $num2){
+    $sum = $num1 + $num2;
+
+    if($sum == 30){
+        echo 'True';
+
+    }else{
+        echo 'False';
+    }
+}
+
+check_number(10,10);
+
+
+b();
+
+
+$num = 20;
+
+if($num % 3 == 0){
+    echo ' True';
 }else{
-    echo "is eligible to vote";
+    echo ' False';
 }
 
 
-echo "<br>";
-echo "<br>";
+b();
 
-$firstInteger = 10;
+$number1 =  50;
 
-if($firstInteger == 0){
-    echo "zero";
-}else if($firstInteger < 0){
-    echo "Negative";
-}else {
-    echo "Postive";
-}
+if ($number1 >= 20 &&  $number1 <= 50){
 
-echo "<br>";
-echo "<br>";
+    echo ' True';
 
-$output = $firstInteger + $secondInteger;
-echo "Addition " .$output ;
-echo "<br>";
-$output = $firstInteger - $secondInteger;
-echo "Subtraction " .$output;
-echo "<br>";
-$output = $firstInteger * $secondInteger;
-echo "Multiplication " .$output;
-echo "<br>";
-$output = $firstInteger / $secondInteger;
-echo "Division " .$output;
-
-echo "<br>";
-echo "<br>";
-
-
-$numbers = array(60,86,95,63,55,74,79,62,50);
-$sum = 0;
-for($i = 0; $i < count($numbers); $i++)
-    $sum += $numbers[$i];
-
-$avg = $sum / count($numbers);
-
-if($avg < 60){
-    echo "F";
-}else if($avg < 70){
-    echo "D";
-}else if($avg < 80){
-    echo "C";
-}else if($avg < 90){
-    echo "B";
-}else if($avg < 100){
-    echo "A";
+}else{
+    echo ' False';
 }
 
 
+b();
 
 
+$nm1 = 8;
+$nm2  = 15;
+$nm3 = 91;
+
+if ($nm1 > $nm2 && $nm1 > $nm3){
+
+    echo $nm1;
+    
+}else if ($nm2 > $nm1 && $nm2 > $nm3){
+    echo $nm2;
+
+}else{
+    echo $nm3;
+}
+
+
+b();
+
+
+$units = 251;
+
+
+if ($units <= 50){
+
+    echo $units * 2.50;
+
+}
+else if($units > 50 && $units <= 100){
+
+    echo $units * 5;
+
+}
+else if ($units > 100 && $units <= 250){
+
+    echo $units * 6.20;
+
+}
+else if ($units > 250){
+
+   echo $units * 7.50;
+
+}
+
+
+b();
+
+
+$vote = 15;
+
+if ($vote > 0 && $vote < 18 ){
+    echo 'it/s not eligible to vote';
+
+}
+else if ($vote >= 18){
+    echo 'it/s a eligible to vote ';
+}else{
+    echo 'this is not right age ';
+}
+
+
+
+b();
+
+$number = -60;
+
+if ($number < 0 ){
+    echo 'Negative  Number';
+}
+else if ($number == 0){
+    echo ' Zero Number ';
+}
+else{
+    echo ' Positive Number ';
+}
+
+b();
+
+$cal1 = 50;
+$cal2 = 12;
+$operation = "Division";
+
+if ($operation == "Addition"){
+    $sum = $cal1 + $cal2;
+    echo $cal1." + ".$cal2." = ".$sum;
+}
+else if ($operation == "Subtraction"){
+    $sub = $cal1 - $cal2;
+    echo $cal1." - ".$cal2." = ".$sub;
+}
+else if ($operation == "Multiplication"){
+    $mult = $cal1 * $cal2;
+    echo $cal1." * ".$cal2." = ".$mult;
+}
+else if ($operation == "Division"){
+    $div = $cal1 / $cal2;
+    echo $cal1." / ".$cal2." = ".$div;
+}else{
+    echo 'this is not true operation';
+}
+
+b();
+
+ $numbers = array(60,86,95,63,90,84,79,62,50);
+ $sum = 0;
+ for($i = 0; $i < count($numbers); $i++)
+     $sum += $numbers[$i];
+
+ $avg = $sum / count($numbers);
+
+ if($avg < 60){
+     echo  $avg." F";
+ }
+ else if($avg < 70){
+     echo $avg." D";  
+ }
+ else if($avg < 80){
+     echo $avg." C";
+ }
+ else if($avg < 90){
+    echo  $avg." B";
+ }
+ else if($avg < 100){
+    echo  $avg." A";
+ }
